4085c0c45e2c1461fabecebc4e8215b73fddabb0
Chris Vik <maxvram@chrisvik.com>
2023-03-05 13:23:57 +1100

    Cleaned up input selector drawer before moving on

--- Assets/_Project/Editor/Scripts/Editor/ModulationInputDrawer.cs
+++ Assets/_Project/Editor/Scripts/Editor/ModulationInputDrawer.cs
@@ -1,41 +1,14 @@
-﻿using System;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
-using UnityEditor.UIElements;
-using UnityEngine.UIElements;
 
 
 namespace PlaneWaver.Parameters
 {
     [CustomPropertyDrawer(typeof(ModulationInputObject))]
-    public class ModulationInputDrawerUIE : PropertyDrawer
+    public class ModulationInputDrawer : PropertyDrawer
     {
         private SerializedProperty _inputGroup, _miscInput, _actorInput, _relativeInput, _collisionInput;
         
-        // Not complete/implemented
-        public override VisualElement CreatePropertyGUI(SerializedProperty property)
-        {
-            _inputGroup = property.FindPropertyRelative("InputGroup");
-            _miscInput = property.FindPropertyRelative("MiscInput");
-            _actorInput = property.FindPropertyRelative("ActorInput");
-            _relativeInput = property.FindPropertyRelative("RelativeInput");
-            _collisionInput = property.FindPropertyRelative("CollisionInput");
-            
-            var container = new VisualElement();
-            var label = new Label(property.displayName);
-            container.Add(label);
-
-            var groupValue = (InputGroups) _inputGroup.enumValueIndex;
-            var miscValue = (InputMisc) _miscInput.enumValueIndex;
-            var actorValue = (InputActor) _actorInput.enumValueIndex;
-            var relativeValue = (InputRelative) _relativeInput.enumValueIndex;
-            var collisionValue = (InputCollision) _collisionInput.enumValueIndex;    
-            
-            var inputGroup = new EnumField("Input Type", groupValue);
-            container.Add(inputGroup);
-            return container;
-        }
-
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             int indent = EditorGUI.indentLevel;
